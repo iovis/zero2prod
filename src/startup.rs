8ee0ff5b80cd740de06d916435bfe3f78e7902ea
David Marchante <davidmarchan@gmail.com>
2022-04-17 04:25:13 -0400

    Inject EmailClient instance

--- src/startup.rs
+++ src/startup.rs
@@ -5,11 +5,17 @@ use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
+use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
     // web::Data wraps stuff in an ARC pointer, making it "cloneable"
     let db_pool = web::Data::new(db_pool);
+    let email_client = web::Data::new(email_client);
 
     let server = HttpServer::new(move || {
         App::new()
@@ -17,6 +23,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,6 +4,7 @@ use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::email_client::EmailClient;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -36,8 +37,14 @@ async fn spawn_app() -> TestApp {
     configuration.database.database_name = Uuid::new_v4().to_string();
     let db_pool = configure_database(&configuration.database).await;
 
-    let server =
-        zero2prod::startup::run(listener, db_pool.clone()).expect("Failed to bind address.");
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+
+    let server = zero2prod::startup::run(listener, db_pool.clone(), email_client)
+        .expect("Failed to bind address.");
     let _ = tokio::spawn(server);
 
     TestApp { address, db_pool }
@@ -157,6 +164,6 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             response.status().as_u16(),
             "The API did not return a 400 Bad Request when the payload was {}",
             description
-            );
+        );
     }
 }
