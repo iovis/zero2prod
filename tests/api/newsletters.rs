22963e2c19acc9b6fb5b07aa41839fe3b42517eb
David Marchante <davidmarchan@gmail.com>
2022-04-23 17:42:08 -0400

    Validate JSON body for /newsletters

--- tests/api/newsletters.rs
+++ tests/api/newsletters.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -33,7 +35,7 @@ async fn newsletteres_are_not_delivered_to_unconfirmed_subscribers() {
 }
 
 #[tokio::test]
-async fn newsletteres_aret_delivered_to_confirmed_subscribers() {
+async fn newsletteres_are_delivered_to_confirmed_subscribers() {
     let app = spawn_app().await;
     create_confirmed_subscriber(&app).await;
 
@@ -59,6 +61,42 @@ async fn newsletteres_aret_delivered_to_confirmed_subscribers() {
         .expect("Failed to execute request.");
 
     assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn newsletters_returns_400_for_invalid_data() {
+    let app = spawn_app().await;
+    let test_cases = vec![
+        (
+            serde_json::json!({
+                "content": {
+                    "text": "Newsletter body as plain text",
+                    "html": "<p>Newsletter body as HTML</p>"
+                }
+            }),
+            "missing title",
+        ),
+        (
+            serde_json::json!({ "title": "Newsletter!" }),
+            "missing content",
+        ),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = reqwest::Client::new()
+            .post(format!("{}/newsletters", &app.address))
+            .json(&invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
 }
 
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
