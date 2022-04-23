1206059170718b22fc3539bafc7712c44d9d49db
David Marchante <davidmarchan@gmail.com>
2022-04-23 13:39:35 -0400

    Add error handling if there's a problem storing the token

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -117,3 +117,19 @@ async fn subscribe_sends_confirmation_email_with_a_valid_link() {
 
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
+
+#[tokio::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Sabotage the database
+    sqlx::query!("alter table subscriptions_tokens drop column subscription_token;")
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    let response = app.post_subscriptions(body.into()).await;
+
+    assert_eq!(response.status().as_u16(), 500);
+}
