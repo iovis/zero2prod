b37c97149a4a0ad7cc78ec2bc44150f69694e97c
David Marchante <davidmarchan@gmail.com>
2022-04-23 21:06:23 -0400

    Add homepage

--- src/startup.rs
+++ src/startup.rs
@@ -8,7 +8,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, subscribe, publish_newsletter};
+use crate::routes::{confirm, health_check, home, publish_newsletter, subscribe};
 
 pub struct Application {
     port: u16,
@@ -84,6 +84,7 @@ pub fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/", web::get().to(home))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
