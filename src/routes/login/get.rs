9ac28e4ba8480c46a310715dea2b360387c14d54
David Marchante <davidmarchan@gmail.com>
2022-04-24 23:00:01 -0400

    Revert get login functionality

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,15 +1,7 @@
 use actix_web::http::header::ContentType;
-use actix_web::{HttpResponse, web};
-use serde::Deserialize;
+use actix_web::HttpResponse;
 
-#[derive(Deserialize)]
-pub struct QueryParams {
-    error: Option<String>,
-}
-
-pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
-    let _error = query.0.error;
-
+pub async fn login_form() -> HttpResponse {
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(include_str!("login.html"))
