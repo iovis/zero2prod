4ce8e4743f12833d16a0d8079df6add3b0af9a80
David Marchante <davidmarchan@gmail.com>
2022-04-24 23:08:30 -0400

    Show flash message to the user

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,48 @@
 use actix_web::http::header::ContentType;
-use actix_web::HttpResponse;
+use actix_web::{HttpRequest, HttpResponse};
 
-pub async fn login_form() -> HttpResponse {
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
+        None => "".into(),
+        Some(cookie) => format!("<p><i>{}</i></p>", cookie.value()),
+    };
+
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("login.html"))
+        .body(format!(
+            r#"
+            <!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8">
+                <title>Login</title>
+            </head>
+
+            <body>
+                {error_html}
+
+                <form action="/login" method="post">
+                    <label>Username
+                        <input
+                            type="text"
+                            placeholder="Enter Username"
+                            name="username"
+                        >
+                    </label>
+
+                    <label>Password
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                        >
+                    </label>
+
+                    <button type="submit">Login</button>
+                </form>
+            </body>
+            </html>
+            "#,
+        ))
 }
