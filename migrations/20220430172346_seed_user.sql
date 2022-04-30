a7bf65ac9deb92fd3c2834bcc47067834b698275
David Marchante <davidmarchan@gmail.com>
2022-04-30 19:48:04 -0400

    Seed user

---
+++ migrations/20220430172346_seed_user.sql
@@ -0,0 +1,6 @@
+insert into users (user_id, username, password_hash)
+values (
+  '83cc8885-6d0e-486a-a034-9f068237c3fe',
+  'admin',
+  '$argon2id$v=19$m=15000,t=2,p=1$Lk8W5p2f08FQjTEy8Fn4gA$kst1pTf4llAn8UEVjnq/mqDew4Jos1eK89rc8aBRaYs'
+);
