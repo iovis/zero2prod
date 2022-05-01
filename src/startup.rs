adcac743ab1080e9cd9549dcb76343a468ec43d0
David Marchante <davidmarchan@gmail.com>
2022-05-01 14:48:06 -0400

    Implement logout

--- src/startup.rs
+++ src/startup.rs
@@ -15,7 +15,7 @@ use tracing_actix_web::TracingLogger;
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{
-    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe, change_password, change_password_form,
+    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe, change_password, change_password_form, log_out,
 };
 
 pub struct Application {
@@ -116,6 +116,7 @@ pub async fn run(
             .route("/admin/dashboard", web::get().to(admin_dashboard))
             .route("/admin/password", web::get().to(change_password_form))
             .route("/admin/password", web::post().to(change_password))
+            .route("/admin/logout", web::post().to(log_out))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
--- tests/api/admin_dashboard.rs
+++ tests/api/admin_dashboard.rs
@@ -1,3 +1,5 @@
+use serde_json::json;
+
 use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
@@ -6,5 +8,29 @@ async fn you_must_be_logged_in_to_access_the_admin_dashboard() {
 
     let response = app.get_admin_dashboard().await;
 
+    assert_is_redirect_to(&response, "/login");
+}
+
+#[tokio::test]
+async fn logout_clears_session_state() {
+    let app = spawn_app().await;
+
+    let login_body = json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    });
+    let response = app.post_login(&login_body).await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+
+    let html_page = app.get_admin_dashboard_html().await;
+    assert!(html_page.contains(&format!("Welcome {}", app.test_user.username)));
+
+    let response = app.post_logout().await;
+    assert_is_redirect_to(&response, "/login");
+
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains(r#"<p><i>You have successfully logged out.</i></p>"#));
+
+    let response = app.get_admin_dashboard().await;
     assert_is_redirect_to(&response, "/login");
 }
