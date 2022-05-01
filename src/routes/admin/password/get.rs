9c2ac38b0467ff39524613a8672386309de5e118
David Marchante <davidmarchan@gmail.com>
2022-05-01 14:12:57 -0400

    Show message when passwords don't match

--- src/routes/admin/password/get.rs
+++ src/routes/admin/password/get.rs
@@ -1,48 +1,61 @@
 use actix_web::http::header::ContentType;
 use actix_web::HttpResponse;
+use actix_web_flash_messages::IncomingFlashMessages;
+use std::fmt::Write;
 
 use crate::session_state::TypedSession;
 use crate::utils::{error500, see_other};
 
-pub async fn change_password_form(session: TypedSession) -> Result<HttpResponse, actix_web::Error> {
+pub async fn change_password_form(
+    session: TypedSession,
+    flash_messages: IncomingFlashMessages,
+) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(error500)?.is_none() {
         return Ok(see_other("/login"));
     };
 
+    let mut msg_html = String::new();
+
+    for message in flash_messages.iter() {
+        writeln!(msg_html, "<p><i>{}</i></p>", message.content()).unwrap();
+    }
+
     Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(HTML_FORM))
+        .body(format!(
+            r#"
+            <!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+              <title>Change Password</title>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+
+            <body>
+              {msg_html}
+
+              <form action="/admin/password">
+                <label>Current Password
+                  <input type="password" placeholder="Enter current password" name="current_password">
+                </label>
+                <br>
+                <label>New Password
+                  <input type="password" placeholder="Enter new password" name="new_password">
+                </label>
+                <br>
+                <label>Confirm new Password
+                  <input type="password" placeholder="Confirm your password" name="new_password_check">
+                </label>
+
+                <button type="submit">Change password</button>
+              </form>
+
+              <p><a href="/admin/dashboard">&lt;- Back</a></p>
+            </body>
+
+            </html>
+            "#
+        )))
 }
-
-const HTML_FORM: &str = r#"
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <title>Change Password</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-
-<body>
-  <form action="/admin/password">
-    <label>Current Password
-      <input type="password" placeholder="Enter current password" name="current_password">
-    </label>
-    <br>
-    <label>New Password
-      <input type="password" placeholder="Enter new password" name="new_password">
-    </label>
-    <br>
-    <label>Confirm new Password
-      <input type="password" placeholder="Confirm your password" name="new_password_check">
-    </label>
-
-    <button type="submit">Change password</button>
-  </form>
-
-  <p><a href="/admin/dashboard">&lt;- Back</a></p>
-</body>
-
-</html>
-"#;
--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -1,3 +1,5 @@
+use uuid::Uuid;
+
 use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
@@ -7,4 +9,32 @@ async fn you_must_be_logged_in_to_see_the_change_password_form() {
     let response = app.get_change_password().await;
 
     assert_is_redirect_to(&response, "/login");
+}
+
+#[tokio::test]
+async fn new_password_fields_must_match() {
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let another_new_password = Uuid::new_v4().to_string();
+
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    }))
+    .await;
+
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &app.test_user.password,
+            "new_password": &new_password,
+            "new_password_check": &another_new_password,
+        }))
+        .await;
+
+    assert_is_redirect_to(&response, "/admin/password");
+
+    let html_page = app.get_change_password().await.text().await.unwrap();
+    assert!(html_page.contains(
+        "<p><i>You entered two different new passwords - the field values must match.</i></p>"
+    ))
 }
